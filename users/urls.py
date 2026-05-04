b27d1c3e408add820e90af5dab07567c9f81b4e0
Shohanul Islam <shohanulislam19892@gmail.com>
2026-05-05 05:56:26 +0600

    Update users/urls.py: Added dashboard/ and health-sync/ API endpoints

--- users/urls.py
+++ users/urls.py
@@ -22,6 +22,8 @@ from .views import (
     SupportTicketView,
     OtherUserProfileView,
     UserFitnessInfoUpdateView,
+    UserDashboardView,
+    HealthDataUpdateView,
 )
 
 app_name = 'users'
@@ -51,6 +53,8 @@ urlpatterns = [
     path('account-delete/', AccountDeleteView.as_view(), name='account-delete'),
     path('support-ticket/', SupportTicketView.as_view(), name='support-ticket'),
     path('fitness-info/', UserFitnessInfoUpdateView.as_view(), name='fitness-info'),
+    path('dashboard/', UserDashboardView.as_view(), name='dashboard'),
+    path('health-sync/', HealthDataUpdateView.as_view(), name='health-sync'),
     path('profile/<uuid:pk>/', OtherUserProfileView.as_view(), name='other-user-profile'),
 
     # Account Deletion
